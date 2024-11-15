5d30415fe035267183079be0575c3182ca89994e
Calmie <safiullinrafa@yandex.ru>
2024-11-15 19:38:14 +0300

    step 1: Routing pre-final

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,110 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
+import { ProtectedRoute } from '../protected-route/ProtectedRoute';
 
-import { AppHeader } from '@components';
+const App = () => {
+  const navigation = useNavigate();
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='*' element={<NotFound404 />} />
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed'>
+          <Route index element={<Feed />} />
+          <Route
+            path=':number'
+            element={
+              <Modal title='' onClose={() => navigation('/feed')}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Route>
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <ProtectedRoute>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='forgot-password'
+          element={
+            <ProtectedRoute>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='reset-password'
+          element={
+            <ProtectedRoute>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='/profile'>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='orders/:number'
+            element={
+              <ProtectedRoute>
+                <OrderInfo />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal title='' onClose={() => navigation('/')}>
+              {' '}
+              <OrderInfo />{' '}
+            </Modal>
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
